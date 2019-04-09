8df9de467dcb82c49aea3e559da5759be21b3df9
David Pérez <knightbcn@gmail.com>
2019-04-10 00:37:56 +0200

    Primer commit rest server

---
+++ classes/server.ts
@@ -0,0 +1,29 @@
+import { SERVER_PORT } from './../global/environment';
+import express from 'express';
+import socketIO from 'socket.io';
+import http from 'http';
+
+export default class Server {
+	public app: express.Application;
+	public port: number;
+	public io: socketIO.Server;
+	private httpServer: http.Server;
+
+	constructor() {
+		this.app = express();
+		this.port = SERVER_PORT;
+		this.httpServer = new http.Server(this.app);
+		this.io = socketIO(this.httpServer);
+	}
+
+	private escucharSockets() {
+		console.log('Escuchando conexiones - sockets');
+		this.io.on('connection', cliente => {
+			console.log('Cliente conectado');
+		});
+	}
+
+	start(callback: any) {
+		this.httpServer.listen(this.port, callback);
+	}
+}
